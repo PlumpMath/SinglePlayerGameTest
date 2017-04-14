85db921cd1bffd7b234105fbfa18a58e89c1de9d
Craz3dBull <craz3dbullofficial@gmail.com>
2017-04-14 15:18:37 -0700

    Update PlayerToon.py

--- src/PlayerToon.py
+++ src/PlayerToon.py
@@ -8,6 +8,7 @@ import ClothingGlobals
 class PlayerToon():
     def __init__(self):
         self.actor = Toon('player', ToonGlobals.boyTorsoModelDict['s'], ToonGlobals.boyLegsModelDict['s'], ToonGlobals.boyMediumTorsoAnimDict, ToonGlobals.boySmallLegsAnimDict, 's')
+        base.render_pipeline.prepare_scene(self.actor)
         self.charController = characterController(self.actor)
         self.actor.reparentTo(render)
         self.followCam = FollowCam(base.cam, self.actor)
